c78e868f5a35738651c076eb2c2b891b3ec922d4
tayang <tayang@gmail.com>
2023-09-09 14:33:15 +0800

    09/09/2023 2

--- leetcode/editor/cn/[28]Find the Index of the First Occurrence in a String.java
+++ leetcode/editor/cn/[28]Find the Index of the First Occurrence in a String.java
@@ -32,24 +32,58 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int strStr(String haystack, String needle) {
-        int len1 = haystack.length();
-        int len2 = needle.length();
+    public int strStr(String ss, String pp) {
+        //KMP algorithm
+        //ss : haystack pp : needle
+        if (pp.isEmpty()) return 0;
+        //分别读取原串和匹配串的长度
+        int n = ss.length(), m = pp.length();
+        // 原串和匹配串前面都加空格，使其下标从 1 开始
+        ss = " " + ss;
+        pp = " " + pp;
+        char[] s = ss.toCharArray();
+        char[] p = pp.toCharArray();
+        // 构建 next 数组，数组长度为匹配串的长度（next 数组是和匹配串相关的）
 
-        if (len1 < len2) return -1;
-        if (len1 == len2) return haystack.equals(needle) ? 0 : -1;
+        int[] next = new int[m + 1];
+        // 构造过程 i = 2，j = 0 开始，i 小于等于匹配串长度 【构造 i 从 2 开始】
+        for (int i = 2, j = 0; i <= m; i++) {
+            // 匹配不成功的话，j = next(j)，直到匹配成功或者 j = 0 为止
+            while (j > 0 && p[i] != p[j + 1]) j = next[j];
+            // 匹配成功的话，先让 j++
+            if (p[i] == p[j + 1]) j++;
+            // 更新 next[i]，结束本次循环，i++
+            next[i] = j;
+        }
 
-        if (haystack.contains(needle)) {
-            char c = needle.charAt(0);
-            for (int i = 0; i < len1 - len2 + 1; i++) {
-                if (haystack.charAt(i) == c) {
-                    if (haystack.substring(i, i + len2).equals(needle)) {
-                        return i;
-                    }
-                }
-            }
+        // 匹配过程 i = 1，j = 0 开始，i 小于等于原串长度 【匹配 i 从 1 开始】
+        for (int i = 1, j = 0; i <= n; i++) {
+            // 匹配不成功 j = next(j)，直到匹配成功或者 j = 0 为止
+            while (j > 0 && s[i] != p[j + 1]) j = next[j];
+            // 匹配成功的话，先让 j++
+            if (s[i] == p[j + 1]) j++;
+            // 整一段匹配成功，直接返回下标
+            if (j == m) return i - m;
         }
         return -1;
+
+        //        int len1 = haystack.length();
+//        int len2 = needle.length();
+//
+//        if (len1 < len2) return -1;
+//        if (len1 == len2) return haystack.equals(needle) ? 0 : -1;
+//
+//        if (haystack.contains(needle)) {
+//            char c = needle.charAt(0);
+//            for (int i = 0; i < len1 - len2 + 1; i++) {
+//                if (haystack.charAt(i) == c) {
+//                    if (haystack.substring(i, i + len2).equals(needle)) {
+//                        return i;
+//                    }
+//                }
+//            }
+//        }
+//        return -1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
