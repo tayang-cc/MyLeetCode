6f8a5144246ab5f7fc8aa15429b62a8b14715cd6
tayang.cc <tayang.cc@gmail.com>
2023-09-10 12:04:20 +0800

    10/09/2023 1

--- leetcode/editor/cn/[100]Same Tree.java
+++ leetcode/editor/cn/[100]Same Tree.java
@@ -38,25 +38,31 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        boolean res = true;
-
+        // 递归
+        // 1. 递归终止条件
+        if (p == null && q == null) return true;
+        // 2. 递归过程
+        if (p == null || q == null) return false;
+        return p.val == q.val && isSameTree(p.left, q.left)
+                && isSameTree(p.right, q.right);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
