9c1977684f2fb4bdbd971b9c5c3c2662c8f75ff1
tayang.cc <tayang.cc@gmail.com>
2023-09-12 11:20:17 +0800

    12/09/2023 2

---
+++ leetcode/editor/cn/[110]Balanced Binary Tree.java
@@ -0,0 +1,65 @@
+//Given a binary tree, determine if it is height-balanced. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: root = [3,9,20,null,null,15,7]
+//Output: true
+// 
+//
+// Example 2: 
+// 
+// 
+//Input: root = [1,2,2,3,3,null,null,4,4]
+//Output: false
+// 
+//
+// Example 3: 
+//
+// 
+//Input: root = []
+//Output: true
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree is in the range [0, 5000]. 
+// -10⁴ <= Node.val <= 10⁴ 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉树 👍 1402 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        else return Math.abs(height(root.left) - height(root.right)) <= 1
+                && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
