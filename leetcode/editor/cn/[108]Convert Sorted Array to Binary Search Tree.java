3fda4c2593d946491d036543fbd563a476d0c555
tayang.cc <tayang.cc@gmail.com>
2023-09-12 10:55:10 +0800

    12/09/2023 1

---
+++ leetcode/editor/cn/[108]Convert Sorted Array to Binary Search Tree.java
@@ -0,0 +1,65 @@
+//Given an integer array nums where the elements are sorted in ascending order, 
+//convert it to a height-balanced binary search tree. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: nums = [-10,-3,0,5,9]
+//Output: [0,-3,9,-10,null,5]
+//Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+//
+// 
+//
+// Example 2: 
+// 
+// 
+//Input: nums = [1,3]
+//Output: [3,1]
+//Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// nums is sorted in a strictly increasing order. 
+// 
+//
+// Related Topics 树 二叉搜索树 数组 分治 二叉树 👍 1385 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode helper(int[] nums, int left, int right){
+        if(left > right) return null;
+
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
