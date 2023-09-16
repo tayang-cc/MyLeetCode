1a6b0f25ad4162d487acc2a71efa60b113e856be
tayang.cc <tayang.cc@gmail.com>
2023-09-16 23:41:52 +0800

    16/09/2023 1

---
+++ leetcode/editor/cn/[198]House Robber.java
@@ -0,0 +1,63 @@
+//You are a professional robber planning to rob houses along a street. Each 
+//house has a certain amount of money stashed, the only constraint stopping you from 
+//robbing each of them is that adjacent houses have security systems connected and 
+//it will automatically contact the police if two adjacent houses were broken 
+//into on the same night. 
+//
+// Given an integer array nums representing the amount of money of each house, 
+//return the maximum amount of money you can rob tonight without alerting the 
+//police. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [1,2,3,1]
+//Output: 4
+//Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+//Total amount you can rob = 1 + 3 = 4.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [2,7,9,3,1]
+//Output: 12
+//Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 
+//(money = 1).
+//Total amount you can rob = 2 + 9 + 1 = 12.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 100 
+// 0 <= nums[i] <= 400 
+// 
+//
+// Related Topics 数组 动态规划 👍 2770 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        // 子问题：
+        // f(k) = 偷 [0..k) 房间中的最大金额
+        // f(0) = 0
+        // f(1) = nums[0]
+        // f(k) = max{ rob(k-1), nums[k-1] + rob(k-2) }
+        int N = nums.length;
+        int[] dp = new int[N + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int k = 2; k <= N; k++) {
+            dp[k] = Math.max(dp[k - 1], nums[k - 1] + dp[k - 2]);
+        }
+        return dp[N];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
