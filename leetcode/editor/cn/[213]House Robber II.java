714601ed807f6b97b63389fe9b5b55b37670093b
tayang.cc <tayang.cc@gmail.com>
2023-09-17 23:31:54 +0800

    17/09/2023 1

---
+++ leetcode/editor/cn/[213]House Robber II.java
@@ -0,0 +1,71 @@
+//You are a professional robber planning to rob houses along a street. Each 
+//house has a certain amount of money stashed. All houses at this place are arranged 
+//in a circle. That means the first house is the neighbor of the last one. 
+//Meanwhile, adjacent houses have a security system connected, and it will automatically 
+//contact the police if two adjacent houses were broken into on the same night. 
+//
+// Given an integer array nums representing the amount of money of each house, 
+//return the maximum amount of money you can rob tonight without alerting the 
+//police. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [2,3,2]
+//Output: 3
+//Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 
+//2), because they are adjacent houses.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [1,2,3,1]
+//Output: 4
+//Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+//Total amount you can rob = 1 + 3 = 4.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [1,2,3]
+//Output: 3
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 100 
+// 0 <= nums[i] <= 1000 
+// 
+//
+// Related Topics 数组 动态规划 👍 1490 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int rob(int[] nums) {
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        } else if (length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        return Math.max(robRange(nums, 0, length - 2), robRange(nums, 1, length - 1));
+    }
+
+    public int robRange(int[] nums, int start, int end) {
+        int first = nums[start], second = Math.max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
