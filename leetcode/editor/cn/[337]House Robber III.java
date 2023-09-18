0d1cbb5a73cf41c532e2e47155d41df52faba4a9
tayang.cc <tayang.cc@gmail.com>
2023-09-18 14:00:36 +0800

    18/09/2023 1

---
+++ leetcode/editor/cn/[337]House Robber III.java
@@ -0,0 +1,74 @@
+//The thief has found himself a new place for his thievery again. There is only 
+//one entrance to this area, called root. 
+//
+// Besides the root, each house has one and only one parent house. After a tour,
+// the smart thief realized that all houses in this place form a binary tree. It 
+//will automatically contact the police if two directly-linked houses were broken 
+//into on the same night. 
+//
+// Given the root of the binary tree, return the maximum amount of money the 
+//thief can rob without alerting the police. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: root = [3,2,3,null,3,null,1]
+//Output: 7
+//Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+// 
+//
+// Example 2: 
+// 
+// 
+//Input: root = [3,4,5,1,3,null,1]
+//Output: 9
+//Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree is in the range [1, 10⁴]. 
+// 0 <= Node.val <= 10⁴ 
+// 
+//
+// Related Topics 树 深度优先搜索 动态规划 二叉树 👍 1831 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int[] rootStatus = dfs(root);
+        return Math.max(rootStatus[0], rootStatus[1]);
+    }
+
+    public int[] dfs(TreeNode node) {
+        if (node == null) {
+            return new int[]{0, 0};
+        }
+        int[] l = dfs(node.left);
+        int[] r = dfs(node.right);
+        int selected = node.val + l[1] + r[1];
+        int notSelected = Math.max(l[0], l[1]) + Math.max(r[0], r[1]);
+        return new int[]{selected, notSelected};
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
