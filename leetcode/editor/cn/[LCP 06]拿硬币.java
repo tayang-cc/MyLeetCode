d701c01398b5ce8bae46ce9ac965f0f62524c4e5
tayang.cc <tayang.cc@gmail.com>
2023-09-20 20:22:58 +0800

    20/09/2023 1

---
+++ leetcode/editor/cn/[LCP 06]拿硬币.java
@@ -0,0 +1,20 @@
+//English description is not available for the problem. Please switch to 
+//Chinese.
+// Related Topics 数组 数学 👍 112 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minCount(int[] coins) {
+      int count = 0;
+      for (int coin : coins) {
+        if(coin % 2 == 0) {
+          count += coin / 2;
+        } else {
+          count += (coin-1) / 2 + 1;
+        }
+      }
+      return count;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
