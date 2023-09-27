845bd8adea73935bf8014e98160d4f6acf80a98a
tayang.cc <tayang.cc@gmail.com>
2023-09-27 19:44:02 +0800

    27/09/2023 1

---
+++ leetcode/editor/cn/[1333]Filter Restaurants by Vegan-Friendly, Price and Distance.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> filterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance) {
+        int n = restaurants.length;
+        List<int[]> filtered = new ArrayList<int[]>();
+        for (int i = 0; i < n; i++) {
+            if (restaurants[i][3] <= maxPrice && restaurants[i][4] <= maxDistance
+                    && !(veganFriendly == 1 && restaurants[i][2] == 0)) {
+                filtered.add(restaurants[i]);
+            }
+        }
+        Collections.sort(filtered, (a, b) -> {
+            if (a[1] != b[1]) {
+                return b[1] - a[1];
+            } else {
+                return b[0] - a[0];
+            }
+        });
+        List<Integer> res = new ArrayList<Integer>();
+        for (int[] v : filtered) {
+            res.add(v[0]);
+        }
+        return res;
+    }
+}
