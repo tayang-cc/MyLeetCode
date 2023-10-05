86f4c0d5449965c6accf1688d1e4e1c24d8066f9
tayang.cc <tayang.cc@gmail.com>
2023-10-05 14:24:41 +0800

    10/05/2023 1

--- leetcode/editor/cn/[309]Best Time to Buy and Sell Stock with Cooldown.java
+++ leetcode/editor/cn/[309]Best Time to Buy and Sell Stock with Cooldown.java
@@ -43,19 +43,23 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices.length <= 1) return 0;
-        int[][] dp = new int[prices.length][2];
-        // dp[i][0]表示第i天持有股票的最大收益，dp[i][1]表示第i天不持有股票的最大收益
-        dp[0][0] = -prices[0]; dp[0][1] = 0;
-        dp[1][0] = Math.max(dp[0][0], -prices[1]); dp[1][1] = Math.max(dp[0][1], dp[0][0] + prices[1]);
-        for(int i = 2; i < prices.length; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 2][1] - prices[i]);
-            // 第i天持有股票的最大收益 = max(第i-1天持有股票的最大收益，第i-2天不持有股票的最大收益 - 第i天股票价格)
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
-            // 第i天不持有股票的最大收益 = max(第i-1天不持有股票的最大收益，第i-1天持有股票的最大收益 + 第i天股票价格)
+        if (prices.length <= 1) {
+            return 0;
         }
-        return dp[prices.length - 1][1];
 
+        int n = prices.length;
+        // f[i][0]: 手上持有股票的最大收益
+        // f[i][1]: 手上不持有股票，并且处于冷冻期中的累计最大收益
+        // f[i][2]: 手上不持有股票，并且不在冷冻期中的累计最大收益
+        int[][] f = new int[n][3];
+        f[0][0] = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            f[i][0] = Math.max(f[i - 1][0], f[i - 1][2] - prices[i]);
+            f[i][1] = f[i - 1][0] + prices[i];
+            f[i][2] = Math.max(f[i - 1][1], f[i - 1][2]);
+        }
+        return Math.max(f[n - 1][1], f[n - 1][2]);
     }
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
